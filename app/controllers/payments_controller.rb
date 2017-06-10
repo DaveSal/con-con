92b61b9515bc7e1dbb7b8daa7ced0c953ebb63c6
Marat <e66m@ya.ru>
2017-06-10 21:55:36 +0600

    Charge fee on every payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,10 +2,13 @@ class PaymentsController < ApplicationController
   def create
     publication = Publication.find(params[:publication])
 
+    fee = publication.price * BigDecimal.new(ENV['FEE_PERCENTAGE'])
+
     charge_params = [{
       amount: publication.price_in_cents,
       currency: 'usd',
-      source: params['stripeToken']
+      source: params['stripeToken'],
+      application_fee: (fee * 100).to_i
     }, stripe_account: publication.author.stripe_user_id]
 
     logger.info("Making charge with following params: #{charge_params}")
