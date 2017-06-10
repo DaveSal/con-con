f0c371119f22488c1fcbf52d20f0edd3a156ce22
dsalamau <salamaudavid@gmail.com>
2017-06-10 18:02:36 +0600

    Добавил js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require vendor/modernizr-2.6.2.min.js
+//= require owl.carousel.min.js
+//= require wow.min.js
+//= require slider.js
+//= require jquery.fancybox.js
+//= require main.js
 //= require custom.js
 //= require_tree .
