87b253ffaabf9deea84d01ae4d5c9afafd70b6b9
dsalamau <salamaudavid@gmail.com>
2017-06-11 13:54:12 +0600

    Добавил title для страницы

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,15 +5,20 @@ class HomeController < ApplicationController
   end
 
   def about
+    @title = 'О проекте'
   end
 
   def feed
+    @title = 'Последние публикации'
+
     publications = Publication.not_sold.order(created_at: 'desc')
 
     render locals: { publications: publications }
   end
 
   def profile
+    @title = 'Личный профиль'
+
     if current_individual.can_sell?
       publications = current_individual.publications.not_sold.order(created_at: 'desc')
       sold_publications = current_individual.publications.sold.order(created_at: 'desc')
